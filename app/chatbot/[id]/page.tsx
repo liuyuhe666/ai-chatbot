ab75a3fc2feb022ce8870cbe313c8bbb0fdfb76e
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2025-04-26 20:12:15 +0800

    build: fix build error

--- app/chatbot/[id]/page.tsx
+++ app/chatbot/[id]/page.tsx
@@ -3,7 +3,7 @@ import type { ChatbotItem } from '@/types/chatbot'
 import Chatbot from '@/components/chatbot'
 import data from '@/data/chatbot.json'
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const chatbotList = data as ChatbotItem[]
   const chatbot = chatbotList.find(item => item.id === Number(id))
